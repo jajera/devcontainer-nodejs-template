03f4870e09d60ecd751b4eed754dd97f5b4461d4
John Ajera <37360952+jajera@users.noreply.github.com>
2024-07-21 12:48:56 +1200

    feat: initial commit

--- hello/app.js
+++ hello/app.js
@@ -1,19 +1,4 @@
-# devcontainer-nodejs-template
-
-```bash
-mkdir hello
-```
-
-```bash
-cd hello
-```
-
-```bash
-npm init -y
-```
-
-```bash
-echo '// app.js
+// app.js
 const http = require("http");
 
 const hostname = "127.0.0.1";
@@ -27,9 +12,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});' > app.js
-```
-
-```bash
-node app.js
-```
+});
